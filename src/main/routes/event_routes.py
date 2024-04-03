472063c646ec217d9972b931ac78ceff02fe2300
MarianaMendanha <mariana.mendanha.mm@gmail.com>
2024-04-02 23:37:57 -0300

    improv: Adding request and response to create event

--- src/main/routes/event_routes.py
+++ src/main/routes/event_routes.py
@@ -1,6 +1,7 @@
 # Importação do módulo Blueprint da biblioteca Flask
 from flask import Blueprint, jsonify, request
 from src.http_types.http_request import HttpRequest
+from src.data.event_handler import EventHandler
 
 # Criação de um Blueprint chamado "event_route" para as rotas relacionadas a eventos
 event_route_bp = Blueprint("event_route", __name__)
@@ -11,4 +12,7 @@ event_route_bp = Blueprint("event_route", __name__)
 @event_route_bp.route("/events", methods=["POST"])
 def create_event():
     http_request = HttpRequest(body=request.json)
-    return jsonify({"ola": "mundo"}), 200
+    event_handler = EventHandler()
+    http_response = event_handler.register(http_request)
+
+    return jsonify(http_response.body), http_response.status_code
